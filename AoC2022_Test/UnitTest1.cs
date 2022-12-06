31afa83ab88f8e131909e0ff355703595578f322
Maciej <maciej@szczerkowski.com>
2022-12-06 11:52:06 +0100

    Initial project setup

---
+++ AoC2022_Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace AoC2022_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+            dss
+        }
+    }
+}
