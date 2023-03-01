3801c79dce1698a04f8f0bc5091930a2fd28be67
Maciej <maciej@szczerkowski.com>
2023-03-01 13:39:19 +0100

    Day 09. Part 2

--- AoC2022/Day09.cs
+++ AoC2022/Day09.cs
@@ -7,104 +7,101 @@ namespace AoC.AoC2022
 {
     internal class Day09 : AoC<List<string>, int, int>
     {
-        private readonly List<Point> _positions;
-        private Point _head;
-        private Point _tail;
-
         public Day09(string dayName) : base(dayName)
         {
-            _positions = new List<Point>();
-            _head = new Point(0, 0);
-            _tail = new Point(0, 0);
-            _positions.Add(_tail);
         }
 
         public override int CalculatePart1()
         {
+            var positions = new List<Point>();
+            var head = new Point(0, 0);
+            var tail = new Point(0, 0);
+            positions.Add(tail);
+
             foreach (var motion in InputData)
             {
                 var command = motion.Split(" ");
                 var direction = command[0];
                 var steps = int.Parse(command[1]);
 
-                MoveHead(direction, steps, ref _head, ref _tail);
-            }
-
-            return _positions.Count;
-        }
-
-        private void MoveHead(string direction, int steps, ref Point head, ref Point tail)
-        {
-            for (var i = 0; i < steps; i++)
-            {
-                switch (direction)
+                for (var i = 0; i < steps; i++)
                 {
-                    case "R":
-                        head.X++;
-                        break;
-                    case "U":
-                        head.Y++;
-                        break;
-                    case "L":
-                        head.X--;
-                        break;
-                    case "D":
-                        head.Y--;
-                        break;
+                    MoveHead(direction, ref head);
+                    if (!PointsTouching(head, tail)) FollowHead(head, ref tail, direction);
+                    if (!positions.Contains(tail)) positions.Add(tail);
                 }
+            }
 
-                if (!PointsTouching(head, tail)) FollowHead(head, ref _tail, direction);
+            return positions.Count;
+        }
 
-                if (!_positions.Contains(tail)) _positions.Add(tail);
+        private static void MoveHead(string direction, ref Point head)
+        {
+            switch (direction)
+            {
+                case "R":
+                    head.X++;
+                    break;
+                case "U":
+                    head.Y++;
+                    break;
+                case "L":
+                    head.X--;
+                    break;
+                case "D":
+                    head.Y--;
+                    break;
             }
         }
+
 
         private static void FollowHead(Point head, ref Point tail, string direction)
         {
-            if (head.Y == tail.Y && (direction == "R" || direction == "L"))
-            {
-                if (direction == "R") tail.X++;
-                else tail.X--;
-            }
-            else if (head.X == tail.X && (direction == "U" || direction == "D"))
-            {
-                if (direction == "U") tail.Y++;
-                else tail.Y--;
-            }
-            else if (head.X > tail.X && head.Y > tail.Y)
-            {
-                tail.X++;
-                tail.Y++;
-            }
-            else if (head.X > tail.X && head.Y < tail.Y)
-            {
-                tail.X++;
-                tail.Y--;
-            }
-            else if (head.X < tail.X && head.Y > tail.Y)
-            {
-                tail.X--;
-                tail.Y++;
-            }
-            else if (head.X < tail.X && head.Y < tail.Y)
-            {
-                tail.X--;
-                tail.Y--;
-            }
+            if (head.X > tail.X) tail.X++;
+            if (head.X < tail.X) tail.X--;
+
+            if (head.Y > tail.Y) tail.Y++;
+            if (head.Y < tail.Y) tail.Y--;
         }
 
         private static bool PointsTouching(Point head, Point tail)
         {
-            return (head.X == tail.X && Math.Abs(head.Y - tail.Y) <= 1) || (head.Y == tail.Y &&
-                                                                            Math.Abs(head.X - tail.X) <= 1)
-                                                                        || (head.X != tail.X && head.Y != tail.Y &&
-                                                                            Math.Abs(head.X - tail.X) == 1 &&
-                                                                            Math.Abs(head.Y - tail.Y) == 1);
+            return Math.Abs(head.Y - tail.Y) <= 1 && Math.Abs(head.X - tail.X) <= 1;
         }
 
         public override int CalculatePart2()
         {
-            return -2;
+            var tailPositions = new List<Point>();
+            var knotsPositions = new List<Point>(new Point[10]);
+            tailPositions.Add(knotsPositions[9]);
+
+            foreach (var motion in InputData)
+            {
+                var command = motion.Split(" ");
+                var direction = command[0];
+                var steps = int.Parse(command[1]);
+
+                for (var i = 0; i < steps; i++)
+                {
+                    var head = knotsPositions[0];
+                    MoveHead(direction, ref head);
+                    knotsPositions[0] = head;
+
+                    for (var knot = 0; knot < 9; knot++)
+                    {
+                        var tmpHead = knotsPositions[knot];
+                        var tmpTail = knotsPositions[knot + 1];
+                        if (!PointsTouching(tmpHead, tmpTail))
+                            FollowHead(tmpHead, ref tmpTail, direction);
+                        knotsPositions[knot + 1] = tmpTail;
+                    }
+
+                    if (!tailPositions.Contains(knotsPositions[9]))
+                        tailPositions.Add(knotsPositions[9]);
+                }
+            }
+
+            return tailPositions.Count;
         }
     }
 }
--- AoC2022_Test/TestDay09.cs
+++ AoC2022_Test/TestDay09.cs
@@ -7,12 +7,15 @@ namespace AoC.AoC2022Test
     public class TestDay09
     {
         private const string TestDayNumber = "Test_Day09";
+        private const string TestDayNumberPart2 = "Test_Day09_Part2";
         private Day09 _day;
+        private Day09 _dayPart2;
 
         [SetUp]
         public void Setup()
         {
             _day = new Day09(TestDayNumber);
+            _dayPart2 = new Day09(TestDayNumberPart2);
         }
 
         [Test]
@@ -24,7 +27,7 @@ namespace AoC.AoC2022Test
         [Test]
         public void Day09_Part2_EndToEnd()
         {
-            Assert.That(_day.CalculatePart2(), Is.EqualTo(8));
+            Assert.That(_dayPart2.CalculatePart2(), Is.EqualTo(36));
         }
     }
 }
