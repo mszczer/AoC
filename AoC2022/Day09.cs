6ce2298fa2261962a7c9d2c47f8eb90edf7c831a
Maciej <maciej@szczerkowski.com>
2023-03-02 09:12:57 +0100

    Code cleanup

--- AoC2022/Day09.cs
+++ AoC2022/Day09.cs
@@ -7,13 +7,57 @@ namespace AoC.AoC2022
 {
     internal class Day09 : AoC<List<string>, int, int>
     {
+        private List<Tuple<string, int>> _commands;
+
         public Day09(string dayName) : base(dayName)
         {
+            GetCommands();
+        }
+
+        private void GetCommands()
+        {
+            _commands = new List<Tuple<string, int>>();
+            foreach (var motion in InputData)
+            {
+                var command = motion.Split(" ");
+                var direction = command[0];
+                var steps = int.Parse(command[1]);
+                _commands.Add(new Tuple<string, int>(direction, steps));
+            }
         }
 
         public override int CalculatePart1()
         {
-            return CountTailPositions(InputData, 2);
+            return CountTailPositions(_commands, 2);
+        }
+
+        private static int CountTailPositions(List<Tuple<string, int>> instructions, int numberOfKnots)
+        {
+            var tailPositions = new List<Point>();
+            var knotsPositions = new List<Point>(new Point[numberOfKnots]);
+            tailPositions.Add(knotsPositions[numberOfKnots - 1]);
+
+            foreach (var motion in instructions)
+                for (var i = 0; i < motion.Item2; i++)
+                {
+                    var head = knotsPositions[0];
+                    MoveHead(motion.Item1, ref head);
+                    knotsPositions[0] = head;
+
+                    for (var knot = 0; knot < numberOfKnots - 1; knot++)
+                    {
+                        var tmpHead = knotsPositions[knot];
+                        var tmpTail = knotsPositions[knot + 1];
+                        if (!PointsTouching(tmpHead, tmpTail))
+                            FollowHead(tmpHead, ref tmpTail, motion.Item1);
+                        knotsPositions[knot + 1] = tmpTail;
+                    }
+
+                    if (!tailPositions.Contains(knotsPositions[numberOfKnots - 1]))
+                        tailPositions.Add(knotsPositions[numberOfKnots - 1]);
+                }
+
+            return tailPositions.Count;
         }
 
         private static void MoveHead(string direction, ref Point head)
@@ -53,42 +97,7 @@ namespace AoC.AoC2022
 
         public override int CalculatePart2()
         {
-            return CountTailPositions(InputData, 10);
-        }
-
-        private static int CountTailPositions(List<string> instructions, int numberOfKnots)
-        {
-            var tailPositions = new List<Point>();
-            var knotsPositions = new List<Point>(new Point[numberOfKnots]);
-            tailPositions.Add(knotsPositions[numberOfKnots - 1]);
-
-            foreach (var motion in instructions)
-            {
-                var command = motion.Split(" ");
-                var direction = command[0];
-                var steps = int.Parse(command[1]);
-
-                for (var i = 0; i < steps; i++)
-                {
-                    var head = knotsPositions[0];
-                    MoveHead(direction, ref head);
-                    knotsPositions[0] = head;
-
-                    for (var knot = 0; knot < numberOfKnots - 1; knot++)
-                    {
-                        var tmpHead = knotsPositions[knot];
-                        var tmpTail = knotsPositions[knot + 1];
-                        if (!PointsTouching(tmpHead, tmpTail))
-                            FollowHead(tmpHead, ref tmpTail, direction);
-                        knotsPositions[knot + 1] = tmpTail;
-                    }
-
-                    if (!tailPositions.Contains(knotsPositions[numberOfKnots - 1]))
-                        tailPositions.Add(knotsPositions[numberOfKnots - 1]);
-                }
-            }
-
-            return tailPositions.Count;
+            return CountTailPositions(_commands, 10);
         }
     }
 }
