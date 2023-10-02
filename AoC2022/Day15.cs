7e1b61b32ced9c84acfc976b6fd32ebc675f7db7
Maciej <maciej@szczerkowski.com>
2023-10-02 11:12:35 +0200

    https://adventofcode.com/2022/day/15 setup

---
+++ AoC2022/Day15.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using AoC.AoC2022.Common;
+
+namespace AoC.AoC2022;
+
+internal class Day15 : AoC<List<string>, int, int>
+{
+    public Day15(string dayName) : base(dayName)
+    {
+    }
+
+    public override int CalculatePart1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int CalculatePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AoC2022_Test/TestDay15.cs
@@ -0,0 +1,30 @@
+﻿using AoC.AoC2022;
+using NUnit.Framework;
+
+namespace AoC.AoC2022Test;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class TestDay15
+{
+    private const string TestDayNumber = "Test_Day15";
+    private Day15 _day;
+
+    [SetUp]
+    public void Setup()
+    {
+        _day = new Day15(TestDayNumber);
+    }
+
+    [Test]
+    public void Day15_Part1_EndToEnd()
+    {
+        Assert.That(_day.CalculatePart1(), Is.EqualTo(26));
+    }
+
+    [Test]
+    public void Day15_Part2_EndToEnd()
+    {
+        Assert.That(_day.CalculatePart2(), Is.EqualTo(140));
+    }
+}
