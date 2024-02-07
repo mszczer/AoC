a82ea64afa9eb403045e89747c9dfcb4a3e2cca6
Maciek <maciej@szczerkowski.com>
2024-02-07 16:50:31 +0100

    Code cleanup and refactoring

--- AoC2023/Day02.cs
+++ AoC2023/Day02.cs
@@ -12,39 +12,17 @@ internal class Day02 : AoC<List<string>, int, int>
 
     public Day02(string dayName) : base(dayName)
     {
-        _gameRecords = GetGameRecords();
+        _gameRecords = ParseGameRecords();
     }
 
-    private Dictionary<int, Draw> GetGameRecords()
+    private Dictionary<int, Draw> ParseGameRecords()
     {
         var gameRecords = new Dictionary<int, Draw>();
         var separator = new[] { " ", ":", ",", ";" };
-        
+
         foreach (var game in InputData)
         {
-            var maxRed = 0;
-            var maxGreen = 0;
-            var maxBlue = 0;
-
-            var gameData = game.GetWords(separator);
-
-            for (var i = 2; i < gameData.Length; i += 2)
-            {
-                var quantity = int.Parse(gameData[i]);
-
-                switch (gameData[i + 1])
-                {
-                    case "blue":
-                        maxBlue = quantity > maxBlue ? quantity : maxBlue;
-                        break;
-                    case "red":
-                        maxRed = quantity > maxRed ? quantity : maxRed;
-                        break;
-                    case "green":
-                        maxGreen = quantity > maxGreen ? quantity : maxGreen;
-                        break;
-                }
-            }
+            var (maxRed, maxGreen, maxBlue) = ExtractMaxColors(game, separator);
 
             var maxDraw = new Draw
             {
@@ -53,23 +31,53 @@ internal class Day02 : AoC<List<string>, int, int>
                 Green = maxGreen
             };
 
-            gameRecords.Add(int.Parse(gameData[1]), maxDraw);
+            gameRecords.Add(int.Parse(game.GetWords(separator)[1]), maxDraw);
         }
 
         return gameRecords;
     }
 
+    private static (int maxRed, int maxGreen, int maxBlue) ExtractMaxColors(string game, string[] separator)
+    {
+        var gameData = game.GetWords(separator);
+        var maxRed = 0;
+        var maxGreen = 0;
+        var maxBlue = 0;
+
+        for (var i = 2; i < gameData.Length; i += 2)
+        {
+            var quantity = int.Parse(gameData[i]);
+
+            switch (gameData[i + 1])
+            {
+                case "blue":
+                    maxBlue = Math.Max(quantity, maxBlue);
+                    break;
+                case "red":
+                    maxRed = Math.Max(quantity, maxRed);
+                    break;
+                case "green":
+                    maxGreen = Math.Max(quantity, maxGreen);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unexpected cube color value: " + gameData[i + 1]);
+            }
+        }
+
+        return (maxRed, maxGreen, maxBlue);
+    }
+
     public override int CalculatePart1()
     {
-        return _gameRecords.Where(game => 
+        return _gameRecords.Where(game =>
                 game.Value is { Red: <= RedCubes, Green: <= GreenCubes, Blue: <= BlueCubes })
-                .Sum(game => game.Key);
+            .Sum(game => game.Key);
     }
 
     public override int CalculatePart2()
     {
-        return _gameRecords.Sum(game => 
-            (game.Value.Blue * game.Value.Green * game.Value.Red));
+        return _gameRecords.Sum(game =>
+            game.Value.Blue * game.Value.Green * game.Value.Red);
     }
 
     private struct Draw
